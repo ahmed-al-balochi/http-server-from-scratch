89235997580bf1f4d97759ac7bfcf448c835ff1b
Ahmed Al Balochi <a7mad98.work@gmail.com>
2025-01-02 06:08:08 +0500

    Server can now read Headers

--- http.go
+++ http.go
@@ -13,7 +13,46 @@ import(
     "fmt"
     "syscall"
     "log"
+    "strings"
+    "errors"
 )
+
+func read_request(buf []byte, length int) error{
+
+  var builder strings.Builder
+  n, err := builder.Write(buf[:length])
+  if err != nil {
+    return errors.New("Error writing to builder")
+  }
+  req_str := builder.String()
+
+  fmt.Println(req_str, n)
+
+  path, after_path, _ := strings.Cut(req_str,"HTTP")
+  ver, after_ver, _ := strings.Cut(after_path,"Host")
+  host, after_host, _ := strings.Cut(after_ver,"User")
+  user, after_user, _ := strings.Cut(after_host,"Accept")
+  accept, after_accept, _ := strings.Cut(after_user,"Accept")
+  lang, after_lang, _ := strings.Cut(after_accept,"Accept")
+  encoding, after_enc, _ := strings.Cut(after_lang,"Connection:")
+  connection, after_conn, _ := strings.Cut(after_enc,"Upgrade")
+  _, priority, _ := strings.Cut(after_conn,"Priority")
+
+  if strings.EqualFold(strings.TrimSpace(connection), "close"){
+    return errors.New("Connection close received!")
+  }
+
+  fmt.Printf(path)
+  fmt.Println("HTTP"+ver)
+  fmt.Println("Host"+host)
+  fmt.Println("User"+user)
+  fmt.Println("Accept"+accept)
+  fmt.Println("Accept"+lang)
+  fmt.Println("Accept"+encoding)
+  fmt.Println("connection"+connection)
+  fmt.Println("Priority"+priority)
+  return nil 
+}
 
 func getpeer(nfd int) (Addr [4]byte, Port int, err error){
   peer, err := syscall.Getpeername(nfd)
@@ -33,18 +72,41 @@ func client_handler(client_socket int){
   var recv_buf = make([]byte, 1024)
   
   for{
-    length, _, recv_err := syscall.Recvfrom(client_socket, recv_buf, 0)
-    if length == 0{
-       log.Print("Client Disconnected")
+   length, _, recv_err := syscall.Recvfrom(client_socket, recv_buf, 0)
+
+   if recv_err != nil{
+      log.Print(recv_err)
+   }
+   if length == 0{
+      log.Print("Connection closed by peer!")
+      break	
+   }else if length < 0{
+      log.Print("Connection reset by peer!")
+      break	
+   }else{
+      err := read_request(recv_buf, length)
+      if err != nil{
+        log.Print(err)
+        response := "HTTP/1.1 200 OK\r\n" +
+        "Content-Type: text/plain\r\n" +
+        "Content-Length: 14\r\n" +
+        "\r\n" +
+        "Connection close received, closing...\n"
+
+        _, msg_err := syscall.Write(client_socket, []byte(response))
+       if msg_err != nil {
+         if msg_err == syscall.EPIPE {
+           log.Print("Broken pipe: Client has disconnected.")
+           break
+         }else {
+           log.Print(msg_err)
+         }
+       }
        break
-    }
-    if recv_err != nil{
-       log.Print(recv_err)
-    }
+      }
+   }
 
-    log.Print("Client Buffer: " , string(recv_buf[:length]))
-
-    response := "HTTP/1.1 200 OK\r\n" +
+   response := "HTTP/1.1 200 OK\r\n" +
     "Content-Type: text/plain\r\n" +
     "Content-Length: 14\r\n" +
     "\r\n" +
@@ -52,13 +114,11 @@ func client_handler(client_socket int){
 
     _, msg_err := syscall.Write(client_socket, []byte(response))
     if msg_err != nil {
-      log.Print(msg_err)
       if msg_err == syscall.EPIPE {
         log.Print("Broken pipe: Client has disconnected.")
         break
       }else {
         log.Print(msg_err)
-        break
       }
     }
   }
@@ -75,7 +135,7 @@ func main() {
   }
 
   sa.Port = 3232
-  sa.Addr = [4]byte{127,0,0,1}
+  sa.Addr = [4]byte{192,168,100,43} //{127,0,0,1}
   bind_err := syscall.Bind(fd, &sa)
   if bind_err != nil{
     log.Fatal(bind_err)
@@ -85,7 +145,7 @@ func main() {
   if listen_err != nil{
     log.Fatal(listen_err)
   }
-  fmt.Println("Server is listening on 127.0.0.1:3232")
+  fmt.Println("Server is listening on 192.168.100.43")
 
   for {
     nfd, _, accept_err := syscall.Accept(fd)
@@ -97,7 +157,7 @@ func main() {
     if getPeerErr != nil {
       log.Println("Getpeername error:", getPeerErr)
     } else {
-      fmt.Print("Client connected: %v:%d\n", addr, port)
+      fmt.Println("Client connected: ", addr, port)
     }
 
     response := "HTTP/1.1 200 OK\r\n" +
