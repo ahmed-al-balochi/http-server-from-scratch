882b39fceedc5d2634b650ef0a8edd3833b43592
Ahmed Al Balochi <a7mad98.ksa@gmail.com>
2025-01-03 06:33:43 +0500

    Server now serves an HTML page with an image

--- http.go
+++ http.go
@@ -15,18 +15,138 @@ import(
     "log"
     "strings"
     "errors"
+    "strconv"
+    "os"
 )
 
-func read_request(buf []byte, length int) error{
+
+
+func send_img(conn_socket int) error{
+  img_file, err := os.ReadFile("hey.jpg")
+  if err != nil{
+    body :="Internal Sever error while loading the img file"
+    send_msg("500", body, "close", conn_socket)
+    return errors.New("Internal Sever error while loading the img file")
+  }
+
+  response := fmt.Sprintf("HTTP/1.1 200 OK\r\n" +
+    "Content-Type: img/jpg\r\n" +
+    "Content-Length: %d\r\n" +
+    "Connection: close\r\n" +
+    "\r\n%s",
+    len(img_file),string(img_file))
+
+  _, msg_err := syscall.Write(conn_socket, []byte(response))
+  if msg_err != nil {
+    if msg_err == syscall.EPIPE {
+      return errors.New("Broken pipe: Client has disconnected.")
+    }else {
+      return msg_err
+    }
+  }
+  return nil
+} 
+
+func send_html(conn_socket int) error{
+  html_file, err := os.ReadFile("index.html")
+  if err != nil{
+    body :="Internal Sever error while loading the HTML file"
+    send_msg("500", body, "close", conn_socket)
+    return errors.New("Internal Sever error while loading the HTML file")
+  }
+
+  response := fmt.Sprintf("HTTP/1.1 200 OK\r\n" +
+    "Content-Type: text/html\r\n" +
+    "Content-Length: %d\r\n" +
+    "Connection: close\r\n" +
+    "\r\n%s",
+    len(html_file),string(html_file))
+
+  _, msg_err := syscall.Write(conn_socket, []byte(response))
+  if msg_err != nil {
+    if msg_err == syscall.EPIPE {
+      return errors.New("Broken pipe: Client has disconnected.")
+    }else {
+      return msg_err
+    }
+  }
+  return nil
+} 
+
+func send_msg(code string, body string, conn_state string, conn_socket int) error{
+  body_len := strconv.Itoa(len(body))
+
+  response := "HTTP/1.1 " + code + " OK\r\n" +
+  "Content-Type: text/plain\r\n" +
+  "Content-Length: " + body_len + "\r\n" +
+  "Connection: " + conn_state  + "\r\n" +
+  "\r\n" +
+  body + "\n"
+
+  _, msg_err := syscall.Write(conn_socket, []byte(response))
+  if msg_err != nil {
+    if msg_err == syscall.EPIPE {
+      return errors.New("Broken pipe: Client has disconnected.")
+    }else {
+      return msg_err
+    }
+  }
+  return nil
+}
+
+func get_peer(conn_socket int) (Addr [4]byte, Port int, err error){
+  peer, err := syscall.Getpeername(conn_socket)
+  if err != nil{
+    log.Fatal(err)
+  }
+  peer_data, ok := peer.(*syscall.SockaddrInet4)
+  if !ok {
+     return [4]byte{}, 0, fmt.Errorf("unexpected address type")
+  }
+  Addr = peer_data.Addr
+  Port = peer_data.Port
+  return Addr, Port, nil
+}
+
+func client_handler(conn_socket int){
+  var recv_buf = make([]byte, 1024)
+  
+  for{
+   length, _, recv_err := syscall.Recvfrom(conn_socket, recv_buf, 0)
+
+   if recv_err != nil{
+      log.Print(recv_err)
+   }
+
+   if length == 0{
+      body := "Connection closed by peer!" 
+      send_msg("400", body, "close", conn_socket)
+      break	
+   }else if length < 0{
+      body := "Connection reset by peer!"
+      send_msg("400", body, "close", conn_socket)
+      break	
+   }else{
+      err := read_request(conn_socket, recv_buf, length)
+      if err != nil{
+        log.Print(err)
+	body := "Detected an error, closing connection..."
+        send_msg("400", body, "close", conn_socket)
+        break
+      }
+   }
+  }
+  syscall.Close(conn_socket)
+}
+
+func read_request(conn_socket int, buf []byte, length int) error{
 
   var builder strings.Builder
-  n, err := builder.Write(buf[:length])
+  _, err := builder.Write(buf[:length])
   if err != nil {
     return errors.New("Error writing to builder")
   }
   req_str := builder.String()
-
-  fmt.Println(req_str, n)
 
   path, after_path, _ := strings.Cut(req_str,"HTTP")
   ver, after_ver, _ := strings.Cut(after_path,"Host")
@@ -42,6 +162,10 @@ func read_request(buf []byte, length int) error{
     return errors.New("Connection close received!")
   }
 
+  if !strings.Contains(strings.TrimSpace(ver), "/1.1"){
+    return errors.New("This Server expects HTTP/1.1!")
+  }
+
   fmt.Printf(path)
   fmt.Println("HTTP"+ver)
   fmt.Println("Host"+host)
@@ -51,129 +175,55 @@ func read_request(buf []byte, length int) error{
   fmt.Println("Accept"+encoding)
   fmt.Println("connection"+connection)
   fmt.Println("Priority"+priority)
+
+  if strings.HasPrefix(path, "GET /index.html") {
+      return send_html(conn_socket)
+  } else if strings.HasPrefix(path, "GET /hey.jpg") {
+      return send_img(conn_socket)
+  }
+
+   //body := "Do you smell what The Rock is cooking!"
+   //send_msg("200", body, "close", conn_socket)
+   send_html(conn_socket)
   return nil 
-}
-
-func getpeer(nfd int) (Addr [4]byte, Port int, err error){
-  peer, err := syscall.Getpeername(nfd)
-  if err != nil{
-    log.Fatal(err)
-  }
-  peer_data, ok := peer.(*syscall.SockaddrInet4)
-  if !ok {
-     return [4]byte{}, 0, fmt.Errorf("unexpected address type")
-  }
-  Addr = peer_data.Addr
-  Port = peer_data.Port
-  return Addr, Port, nil
-}
-
-func client_handler(client_socket int){
-  var recv_buf = make([]byte, 1024)
-  
-  for{
-   length, _, recv_err := syscall.Recvfrom(client_socket, recv_buf, 0)
-
-   if recv_err != nil{
-      log.Print(recv_err)
-   }
-   if length == 0{
-      log.Print("Connection closed by peer!")
-      break	
-   }else if length < 0{
-      log.Print("Connection reset by peer!")
-      break	
-   }else{
-      err := read_request(recv_buf, length)
-      if err != nil{
-        log.Print(err)
-        response := "HTTP/1.1 200 OK\r\n" +
-        "Content-Type: text/plain\r\n" +
-        "Content-Length: 14\r\n" +
-        "\r\n" +
-        "Connection close received, closing...\n"
-
-        _, msg_err := syscall.Write(client_socket, []byte(response))
-       if msg_err != nil {
-         if msg_err == syscall.EPIPE {
-           log.Print("Broken pipe: Client has disconnected.")
-           break
-         }else {
-           log.Print(msg_err)
-         }
-       }
-       break
-      }
-   }
-
-   response := "HTTP/1.1 200 OK\r\n" +
-    "Content-Type: text/plain\r\n" +
-    "Content-Length: 14\r\n" +
-    "\r\n" +
-    "Replying to Mon chacu\n"
-
-    _, msg_err := syscall.Write(client_socket, []byte(response))
-    if msg_err != nil {
-      if msg_err == syscall.EPIPE {
-        log.Print("Broken pipe: Client has disconnected.")
-        break
-      }else {
-        log.Print(msg_err)
-      }
-    }
-  }
-  syscall.Close(client_socket)
 }
 
 // To Try: AF_UNSPEC
 func main() {
   var sa syscall.SockaddrInet4
 
-  fd, sock_err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
+  socket, sock_err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
   if sock_err != nil{
     log.Fatal(sock_err)
   }
 
   sa.Port = 3232
-  sa.Addr = [4]byte{192,168,100,43} //{127,0,0,1}
-  bind_err := syscall.Bind(fd, &sa)
+  sa.Addr = [4]byte{127,0,0,1}
+  bind_err := syscall.Bind(socket, &sa)
   if bind_err != nil{
     log.Fatal(bind_err)
   }
 
-  listen_err := syscall.Listen(fd, 10)
+  listen_err := syscall.Listen(socket, 10)
   if listen_err != nil{
     log.Fatal(listen_err)
   }
   fmt.Println("Server is listening on 192.168.100.43")
 
   for {
-    nfd, _, accept_err := syscall.Accept(fd)
+    connection_socket, _, accept_err := syscall.Accept(socket)
     if accept_err != nil{
         log.Fatal(accept_err)
     }
 
-    addr, port, getPeerErr := getpeer(nfd)
+    addr, port, getPeerErr := get_peer(connection_socket)
     if getPeerErr != nil {
       log.Println("Getpeername error:", getPeerErr)
     } else {
       fmt.Println("Client connected: ", addr, port)
     }
 
-    response := "HTTP/1.1 200 OK\r\n" +
-    "Content-Type: text/plain\r\n" +
-    "Content-Length: 14\r\n" +
-    "\r\n" +
-    "Hey Mon Chachos\n"
+    go client_handler(connection_socket)
 
-    _, msg_err := syscall.Write(nfd, []byte(response))
-    if msg_err != nil{
-        log.Fatal(msg_err)
-        syscall.Close(nfd)
-    }
-    
-    go client_handler(nfd)
-    
-    //syscall.Close(nfd)
   }
 }
